3e9ed18a964d7607a66b8f78a2f9a82f555355b7
Martin Elff <elff@gmx.com>
2015-02-14 13:12:49 +0100

    2015-02-14: Added support for horizontal whiskers.

--- pkg/R/panel-whiskers.R
+++ pkg/R/panel-whiskers.R
@@ -51,3 +51,85 @@ panel.vwsk1 <- function(x,y,
   panel.segments(x0=x-offs,x1=x+offs,y0=y1,y1=y1,col=col,lty=lty,...)
 
 }
+
+
+
+panel.hwsk <- function(x,y,groups=NULL,
+                       subscripts=NULL,
+                       col = superpose.symbol$col,
+                       lty = superpose.line$lty,
+                       lwd = superpose.line$lwd,
+                       box.ratio = 1, box.width = box.ratio/(1 + box.ratio),
+                       width=0,
+                       ...){
+  
+  superpose.symbol <- trellis.par.get("superpose.symbol")
+  superpose.line <- trellis.par.get("superpose.line")
+  
+  if(!missing(box.width) || !missing(box.ratio)){
+    superpose.polygon <- trellis.par.get("superpose.polygon")
+    if(missing(col)) col <- superpose.polygon$border
+    if(missing(lty)) lty <- superpose.polygon$lty
+    if(missing(lwd)) lwd <- superpose.polygon$lwd
+  }
+  
+  y <- as.numeric(y)
+  
+  if(!length(groups))
+    panel.hwsk1(x,y,
+                col=col,
+                lty=lty,
+                width=width,...)
+  else{
+    
+    if(length(subscripts))
+      groups <- groups[subscripts]
+    if (!is.factor(groups)) 
+      groups <- factor(groups)
+    ngrps <- nlevels(groups)
+    
+    x <- split.intv(x,groups)
+    y <- split(y,groups)
+    
+    col <- rep(col,len=length(y))
+    lty <- rep(lty,len=length(y))
+    ltw <- rep(lty,len=length(y))
+    
+    for(g in seq_along(x)){
+      y.g <- y[[g]]
+      
+      if(!missing(box.width) || !missing(box.ratio)){
+        height <- box.width/ngrps
+        y.g <- y.g + height*(g - (ngrps + 1)/2)
+      }
+      
+      panel.hwsk1(x[[g]],y.g,
+                  col=col[g],
+                  lty=lty[g],
+                  lwd=lwd[g],
+                  width=width,...)
+    }
+  }
+}
+
+panel.hwsk1 <- function(x,y,
+                        col,lty,
+                        lwd,
+                        width,...){
+  
+  if(!length(x)) return(NULL)
+  
+  yunit <- diff(range(y))/length(unique(y))
+  width <- width*yunit
+  
+  x0 <- attr(x,"lwr")
+  x1 <- attr(x,"upr")
+  
+  panel.segments(x0=x0,x1=x1,y0=y,y1=y,col=col,lty=lty,lwd=lwd,...)
+  
+  offs <- width/2
+  panel.segments(x0=x0,x1=x0,y0=y-offs,y1=y+offs,col=col,lty=lty,lwd=lwd,...)
+  panel.segments(x0=x1,x1=x1,y0=y-offs,y1=y+offs,col=col,lty=lty,lwd=lwd,...)
+  
+}
+
